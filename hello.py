8e4d8fd76635a0a619364043a5d44e7f4e800fd7
LeeDonguk <1971455@hansung.ac.kr>
2022-10-16 15:05:36 +0900

    "num1 + num2 print"

--- hello.py
+++ hello.py
@@ -2,4 +2,11 @@ print("Hello World")
 print("Tell your World")
 print("Tell his world")
 print("Tello her world")
+num1 = 2
+num2 = 2
 
+print("num1 =", num1)
+print("num2 =", num2)
+
+if ((num1) == num2):
+    print(num1 + num2)
